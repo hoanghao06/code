98c8efb23130480e6ce0b64546ee8e1da64d2acd
Do Huy Tien <107696490+Tienhp03@users.noreply.github.com>
2026-04-12 15:49:57 +0700

    Refactor CarsPath class and improve docstrings

--- arg_data.py
+++ arg_data.py
@@ -9,7 +9,7 @@ cars_height = 2  # m
 all_time = 300
 
 
-# 坐标中心为[0, 0], x:[-500, 500], y:[500, 500]
+#[0, 0], x:[-500, 500], y:[500, 500]
 class CarsPath:
     def __init__(self):
         self.cars_pos = None
@@ -18,10 +18,10 @@ class CarsPath:
 
     def load(self, speed: int, force: int, num: int) -> np.ndarray:
         """
-        :param speed: 车辆速度, [5, 10]
-        :param force: 车辆的力, [5]
-        :param num: 车辆数量, 1-8
-        :return: 初始坐标
+        :param speed: [5, 10]
+        :param force: [5]
+        :param num: 1-8
+        :return: 
         """
         self.cars_num = num
         traces_name = np.random.choice(trace_indices, replace=False)
@@ -33,14 +33,12 @@ class CarsPath:
             path = pre_path + str(i) + "th.csv"
             # path = pre_path + "th.csv"
             data = pd.read_csv(path, usecols=[0, 1]).values
-            # 坐标变换
             data = data - 500.
             self.cars_pos.append(data)
 
         obj_path = "./env/data/speed_" + str(speed) + "_force_" + str(force) + \
                    "/objects_data_Train" + str(traces_name) + "th.csv"
         self.obj_pos = pd.read_csv(obj_path, index_col=0).values
-        # 坐标变换
         self.obj_pos[:, 0:2] -= 500.
         self.obj_pos[:, 3:5] -= 500.
         # self.obj_pos[:, -1] += 5.
@@ -52,9 +50,9 @@ class CarsPath:
 
     def get_inter_distance(self, time: int, point: np.ndarray):
         """
-        :param time: 找到时间
-        :param point: 无人机坐标 (3,)
-        :return: 是否交叉, 汽车坐标, 无人机与各车辆之间的距离 [m]
+        :param time:
+        :param point:  (3,)
+        :return:  [m]
         """
         temp_cars_pos_list = [np.r_[self.cars_pos[i][time, :], cars_height] for i in range(self.cars_num)]
         # print(temp_cars_pos_list)
