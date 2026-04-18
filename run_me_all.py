c2b6d757bc386ef3052bb7ca8bf63bff1f7812fa
Do Huy Tien <107696490+Tienhp03@users.noreply.github.com>
2026-04-18 09:49:46 +0700

    Refactor run_me_all.py for new target rate and seed
    Update target rate and seed value in main function call.

--- run_me_all.py
+++ run_me_all.py
@@ -1,26 +1,24 @@
 from kernel import main
 from train_args import args_list
-import matplotlib.pyplot as plt
 
-target_rate = 1200  # Mbps
+target_rate = 3.5
 
 def func(args, path_num, speed):
-    result = main(args=args, seed=20205598, speed=speed, target_rate=target_rate, 
-                  ROOT_PATH='./output/' + 'speed_' + str(speed) + '/' + str(path_num))
-    return result
+    return main(
+        args=args,
+        seed=1052026,
+        speed=speed,
+        target_rate=target_rate,
+        ROOT_PATH=f'./output/speed_{speed}/{path_num}'
+    )
 
 if __name__ == "__main__":
-    import os
-    import shutil
+    import os, shutil
 
     if os.path.exists('./output'):
         shutil.rmtree('./output')
 
-    times = len(args_list)
     speed = 10
 
-    all_results = []
-    for i in range(times):
-        result = func(args_list[i], i, speed)
-        all_results.append((i, speed, result))
-        # print(f"Result for path {i}, speed {sped}: {result}")
+    for i, args in enumerate(args_list):
+        func(args, i, speed)
